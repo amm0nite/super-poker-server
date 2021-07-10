765ad6b515bf38ee93bb7ae135fa0da3af010cf3
amm0nite <ammonite.myfox@gmail.com>
2021-07-10 18:06:10 +0200

    Proper server close on signal

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ import { Server } from './server.js';
 
 const server = new Server();
 server.start();
+
+process.on('SIGINT', () => {
+    server.stop();
+});
+process.on('SIGTERM', () => {
+    server.stop();
+});
